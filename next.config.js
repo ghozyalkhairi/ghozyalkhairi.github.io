f30a891889815231e26aad634236fed0afffc912
ghozyalkhairi <inighozy@gmail.com>
2023-06-08 00:31:00 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 }
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false
